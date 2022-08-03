e94d431a8e1f4d10a949008f64f87236ddb6b2a3
Danilo <dap4perez@gmail.com>
2022-08-03 18:17:55 -0300

    FOREACH,FUNCTION E ETC

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-alert("Olá mundo")
+// variaveis do formulario
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#form-user-creat [name=gender]:checked");
+var gender = document.querySelector("#exampleInputGenderF");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach((field, index)=>{
+
+    if (field.name == "gender"){
+
+        if(field.checked){
+            console.log("SIM", field)
+        }
+
+    }else{
+
+        console.log("NÃO")
+
+    }
+    
+    
+    //console.log (field.name)
+
+});
