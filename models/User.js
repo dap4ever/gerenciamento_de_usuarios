90692cc3f9702eeb2a7e75dbd1b52cb8b63b5ff3
Danilo Alves <dap4perez@gmail.com>
2022-10-18 11:02:04 -0300

    G32

--- models/User.js
+++ models/User.js
@@ -88,11 +88,15 @@ class User {
 
     getNewID() {
 
-        if (!window.id) window.id = 0;
+        let usersID = parseInt(localStorage.getItem("userID"));
 
-        id++;
+        if (usersID) window.id = 0;
 
-        return id
+        usersID++;
+
+        localStorage.setItem("usersID", usersID);
+
+        return usersID;
 
     }
 
@@ -104,9 +108,9 @@ class User {
 
             users.map(u => {
 
-                if (u._id === this._id) {
+                if (u._id == this._id) {
 
-                    u = this;
+                    Object.assign(u, this)
 
                 }
 
@@ -120,6 +124,24 @@ class User {
 
             users.push(this);
         }
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
+    remove() {
+
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index) => {
+
+            if (this._id == userData._id) {
+
+                users.splice(index, 1)
+
+            }
+
+        });
 
         localStorage.setItem("users", JSON.stringify(users));
 
